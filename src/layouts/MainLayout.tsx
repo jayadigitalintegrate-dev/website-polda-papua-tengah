0940518d124c8b9c90d8322e504a34d0584dd97e
jayadigitalintegrate-dev <jayadigitalintegrate@gmail.com>
2026-07-14 13:07:30 +0900

    refactor: reorganize component structure

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/Footer";
 
 function MainLayout() {
   return (
