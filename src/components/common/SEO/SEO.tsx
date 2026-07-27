f861ad32684e85bee0754cfdc83712d5d97cbd8a
jayadigitalintegrate-dev <jayadigitalintegrate@gmail.com>
2026-07-27 09:39:39 +0900

    feat: enhance SEO with open graph and canonical metadata

--- src/components/common/SEO/SEO.tsx
+++ src/components/common/SEO/SEO.tsx
@@ -4,78 +4,182 @@ interface SEOProps {
   title: string;
   description?: string;
   keywords?: string[];
+  image?: string;
+  url?: string;
+  type?: string;
 }
 
 export default function SEO({
   title,
-  description,
+  description = "",
   keywords = [],
+  image = "/logo.png",
+  url = window.location.href,
+  type = "website",
 }: SEOProps) {
 
   useEffect(() => {
 
     document.title = title;
 
-    if (description) {
-      let metaDescription =
-        document.querySelector(
-          'meta[name="description"]'
-        );
 
-      if (!metaDescription) {
-        metaDescription =
+    const setMeta = (
+      selector: string,
+      attribute: string,
+      value: string
+    ) => {
+
+      let element =
+        document.querySelector(selector);
+
+      if (!element) {
+
+        element =
           document.createElement("meta");
 
-        metaDescription.setAttribute(
-          "name",
-          "description"
-        );
+        document.head.appendChild(element);
 
-        document.head.appendChild(
-          metaDescription
-        );
       }
 
-      metaDescription.setAttribute(
-        "content",
-        description
+      element.setAttribute(
+        attribute,
+        value
       );
-    }
+
+    };
+
+
+    /*
+      BASIC SEO
+    */
+
+    setMeta(
+      'meta[name="description"]',
+      "name",
+      description
+    );
 
 
     if (keywords.length > 0) {
 
-      let metaKeywords =
-        document.querySelector(
-          'meta[name="keywords"]'
-        );
-
-      if (!metaKeywords) {
-
-        metaKeywords =
-          document.createElement("meta");
-
-        metaKeywords.setAttribute(
-          "name",
-          "keywords"
-        );
-
-        document.head.appendChild(
-          metaKeywords
-        );
-      }
-
-      metaKeywords.setAttribute(
-        "content",
+      setMeta(
+        'meta[name="keywords"]',
+        "name",
         keywords.join(", ")
       );
+
     }
+
+
+    /*
+      OPEN GRAPH
+    */
+
+    setMeta(
+      'meta[property="og:title"]',
+      "property",
+      title
+    );
+
+
+    setMeta(
+      'meta[property="og:description"]',
+      "property",
+      description
+    );
+
+
+    setMeta(
+      'meta[property="og:image"]',
+      "property",
+      image
+    );
+
+
+    setMeta(
+      'meta[property="og:type"]',
+      "property",
+      type
+    );
+
+
+    setMeta(
+      'meta[property="og:url"]',
+      "property",
+      url
+    );
+
+
+    /*
+      TWITTER CARD
+    */
+
+    setMeta(
+      'meta[name="twitter:card"]',
+      "name",
+      "summary_large_image"
+    );
+
+
+    setMeta(
+      'meta[name="twitter:title"]',
+      "name",
+      title
+    );
+
+
+    setMeta(
+      'meta[name="twitter:description"]',
+      "name",
+      description
+    );
+
+
+    setMeta(
+      'meta[name="twitter:image"]',
+      "name",
+      image
+    );
+
+
+    /*
+      CANONICAL
+    */
+
+    let canonical =
+      document.querySelector(
+        'link[rel="canonical"]'
+      );
+
+    if (!canonical) {
+
+      canonical =
+        document.createElement("link");
+
+      canonical.setAttribute(
+        "rel",
+        "canonical"
+      );
+
+      document.head.appendChild(
+        canonical
+      );
+
+    }
+
+    canonical.setAttribute(
+      "href",
+      url
+    );
 
 
   }, [
     title,
     description,
     keywords,
+    image,
+    url,
+    type,
   ]);
 
 
