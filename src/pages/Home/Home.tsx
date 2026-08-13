0a5d520b9847b9b45264a94e7bd579eee265841e
jayadigitalintegrate-dev <jayadigitalintegrate@gmail.com>
2026-08-13 09:34:14 +0900

    fix: restore Hero on homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,30 @@
-import Hero from "../../components/Hero/Hero";
+﻿import Hero from "../../components/Hero/Hero";
 import StatisticsBar from "../../components/home/StatisticsBar";
 import QuickAccess from "../../components/home/QuickAccess/QuickAccess";
 import HeadlineNews from "../../components/home/HeadlineNews/HeadlineNews";
 import NewsVideo from "../../components/news/NewsVideo";
-
 import MediaCenter from "../../components/home/MediaCenter";
 
 function Home() {
-  return (
-    <>
-      <Hero />
+    return (
+        <>
+            <Hero />
 
-      <StatisticsBar />
+            <Hero />
 
-      <QuickAccess />
+        <StatisticsBar />
 
-      <HeadlineNews />
+            <QuickAccess />
 
-      <NewsVideo />
+            <HeadlineNews />
 
-      <MediaCenter />
-    </>
-  );
+            <NewsVideo />
+
+            <MediaCenter />
+        </>
+    );
 }
 
 export default Home;
+
+
